4aca7ab50a16a0ee05693d21246764374c2d8f94
Roger Martinez <roger.that.dev@gmail.com>
2024-03-11 21:25:22 -0700

    placeholder auth component

---
+++ src/app/Auth.tsx
@@ -0,0 +1,8 @@
+export default function Auth() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Auth!</h1>
+      </main>
+    );
+  }
+  
